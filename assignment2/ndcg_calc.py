ae8592bf8dcc5a49c9bcd0cbed2824f6afcf0fdf
sesegma <sesegma@gmail.com>
2018-05-26 21:46:00 +0200

    nDCG function correction

--- assignment2/ndcg_calc.py
+++ assignment2/ndcg_calc.py
@@ -16,12 +16,10 @@ def ndcg_calc(train_df, pred_scores):
     IDCG = Maximum possible DCG given the set of bookings/clicks in the training sample.
     
     """
-    eval_df = train_df[['srch_id', 'booking_bool']]
-    eval_df['click_bool'] = train_df['click_bool'] - train_df['booking_bool']
+    eval_df = train_df[['srch_id', 'booking_bool', 'click_bool']]
     eval_df['score'] = pred_scores
 
-    logger = lambda x: np.log2(x + 1)
-    # rank by the score column and calculate the denominator for each entry
+    logger = lambda x: math.log(x + 1, 2)
     eval_df['log_rank'] = eval_df.groupby(by = 'srch_id')['score'].rank(ascending = False).map(logger)
 
     book_dcg = (eval_df['booking_bool'] * 31.0 / eval_df['log_rank']).sum() #where 2 ** 5 - 1.0 = 31.0
@@ -29,13 +27,16 @@ def ndcg_calc(train_df, pred_scores):
     
     click_dcg = (eval_df['click_bool'] * (eval_df['booking_bool'] == 0) / eval_df['log_rank']).sum()
     
-    # Max number of rows per srch_id in training set is 38.
-    # and put in dict {num of rows: IDCG value}.
-    disc = [1.0 / np.log2(i + 1) if i != 0 else 0 for i in range(38)]
-    disc_dict = {i: np.array(disc).cumsum()[i] for i in range(38)}
+    # Max number of clicks in training set is 30.
+    # Calculate the 31 different contributions to IDCG that 0 to 30 clicks have
+    # and put in dict {num of click: IDCG value}.
+    disc = [1.0 / math.log(i + 1, 2) if i != 0 else 0 for i in range(31)]
+    disc_dict = { i: np.array(disc).cumsum()[i] for i in range(31)}
     
     # Map the number of clicks to its IDCG and subtract off any clicks due to bookings
     # since these were accounted for in book_idcg.
-    click_idcg = (eval_df.groupby(by = 'srch_id')['click_bool'].sum().map(disc_dict)).sum()
+    click_idcg = (eval_df.groupby(by = 'srch_id')['click_bool'].sum().map(disc_dict) - eval_df.groupby(by = 'srch_id')['booking_bool'].sum()).sum()
 
     return (book_dcg + click_dcg) / (book_idcg + click_idcg)
+
+
